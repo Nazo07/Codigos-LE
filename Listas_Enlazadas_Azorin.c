75c2e1adda2359acb214df580148ce2d1b27ac83
Nazo07 <116370575+Nazo07@users.noreply.github.com>
2022-11-25 15:12:50 -0300

    Update Listas_Enlazadas_Azorin.c

--- Listas_Enlazadas_Azorin.c
+++ Listas_Enlazadas_Azorin.c
@@ -10,15 +10,23 @@ struct Node {
 //structs se crearon
 int cant = 0;
 
+//Prototipos de funciones
+
 struct Node * crear_nodo(struct Node * );
 void ordenar (struct Node*);
 void printNodes(struct Node*);
 
 int main(){
+    //Declaro un puntero del tipo struct Node que pide memoria con malloc, y será la dirección del dato inicial de la lista.
     struct Node* inicio = malloc(sizeof(struct Node));
+ 
+    //Creo otro puntero del mismo tipo y le asigno la dirección de la memoria pedida anteriormente, por lo que no hace diferencia.
     struct Node* head = inicio;
     int v = 0;
+   
+    //Creo un nodo con el puntero head que antes creé
     head = crear_nodo(head);
+    
     
     printf("inicio =:%p %d", inicio, inicio->data);
     do{
@@ -28,16 +36,19 @@ int main(){
         printf("\n¿Desea seguir creando? 1=si 0=no\n");
         scanf("%d",&v);
     }while(v==1);
+ 
+    //Imprimo los nodos
     printNodes(inicio);
 }
 
+//Función crear_nodo: recibe el último nodo, 
 struct Node * crear_nodo(struct Node * ant){
     cant++;
     printf("Ingrese un valor:\n");
     scanf("%d", &ant->data);
     printf("\n---------------------------------------------------\n");
     printf("ant !=:%p %d\n", ant, ant->data);
-    
+    //ant = puntero anterior
     //Creamos la siguiente direccion de memoria
     struct Node * aux = malloc(sizeof(struct Node));
     //Igulamos el next de la actual struct a la siguiente
@@ -55,7 +66,7 @@ struct Node * crear_nodo(struct Node * ant){
     //a la nueva direccion de memoria.
     return ant;
 }
- 
+ // recibe parametros 
 void intercambiar(struct Node *a, struct Node *b) {
   struct Node temporal = *a;
   a->data = b->data;
@@ -64,13 +75,18 @@ void intercambiar(struct Node *a, struct Node *b) {
  
 //Odenamiento de las structs dependiendo del valor de data
 //que poseean. (Ordenamiento de burbuja)
+
+//inicial es el puntero al primer dato de la lista
 void ordenar (struct Node* inicial){
+    
+    //Backup es un puntero que inicialmente apunta al primero de la lista e irá cambiando de posición a la siguiente con cada ordenamiento.
     struct Node* backup = inicial;
     
+    //El for se encarga de que el proceso se ejecute una cantidad de veces igual a la cantidad de elementos que haya en la lista.
     for (int indiceActual = 0; indiceActual < cant-1; indiceActual++) {
-      // Si el actual es mayor que el que le sigue a la derecha...
+      // Si el actual es mayor que el que le sigue a la derecha en la lista...
       if (backup->data < backup->next->data) {
-        // ...intercambiarlos, es decir, mover el actual a la derecha y el de la derecha al actuial
+        // intercambiar sus valores, es decir, mover el del actual a la derecha y el de la derecha al actuial
         intercambiar(backup, backup->next);
       }
       backup = backup->next;
